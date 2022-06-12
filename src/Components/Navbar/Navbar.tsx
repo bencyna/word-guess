b2c2b01055fc3a01124d716d9884b2cc9fe94d22
Ben Cyna <benjamincyna@gmail.com>
2022-06-12 17:18:56 +0930

    added tailwind navbar simple

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+      <div className="mb-2 sm:mb-0">
+        <a
+          href="/home"
+          className="text-2xl no-underline text-grey-darkest hover:text-blue-dark"
+        >
+          Home
+        </a>
+      </div>
+      <div>
+        <a
+          href="/one"
+          className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+        >
+          One
+        </a>
+        <a
+          href="/two"
+          className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+        >
+          Two
+        </a>
+        <a
+          href="/three"
+          className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
+        >
+          Three
+        </a>
+      </div>
+    </nav>
+  );
 }
