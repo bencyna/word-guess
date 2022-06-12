3fd36ab58a9472f5cef8a73e78606294987b5895
Ben Cyna <benjamincyna@gmail.com>
2022-06-12 17:20:05 +0930

    updated a and href to links and to

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,35 +1,36 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
       <div className="mb-2 sm:mb-0">
-        <a
-          href="/home"
+        <Link
+          to="/home"
           className="text-2xl no-underline text-grey-darkest hover:text-blue-dark"
         >
           Home
-        </a>
+        </Link>
       </div>
       <div>
-        <a
-          href="/one"
+        <Link
+          to="/one"
           className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
         >
           One
-        </a>
-        <a
-          href="/two"
+        </Link>
+        <Link
+          to="/two"
           className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
         >
           Two
-        </a>
-        <a
-          href="/three"
+        </Link>
+        <Link
+          to="/three"
           className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"
         >
           Three
-        </a>
+        </Link>
       </div>
     </nav>
   );
