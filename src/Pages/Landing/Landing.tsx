c177f2def10f792873b5ae482345b66f33f1f097
Ben Cyna <benjamincyna@gmail.com>
2022-06-13 09:42:14 +0930

    adding play button

--- src/Pages/Landing/Landing.tsx
+++ src/Pages/Landing/Landing.tsx
@@ -6,7 +6,9 @@ export default function Landing() {
   return (
     <div>
         <Navbar/>
-        Landing
+        <div id="gameOptions w-full align-center justify-content flex">
+            <button className="flex justify-center px-24 py-12 bg-blue-400">Play now</button>
+        </div>
     </div>
   )
 }
