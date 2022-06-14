e89d4b091a250ecdecb5d303140d65b7e53aad65
Ben Cyna <benjamincyna@gmail.com>
2022-06-14 20:36:39 +0930

    centered button

--- src/Pages/Landing/Landing.tsx
+++ src/Pages/Landing/Landing.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Navbar from "../../Components/Navbar/Navbar.tsx"
-
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar.tsx";
 
 export default function Landing() {
   return (
-    <div>
-        <Navbar/>
-        <div id="gameOptions w-full m-auto">
-            <button className="flex justify-center px-24 py-12 bg-blue-400 border-r-3xl">Play now h</button>
+    <div className="">
+      <Navbar />
+      <div className="w-1/2 m-auto">
+        <div id="gameOptions" className=" w-1/2 align-center justify-content m-auto">
+          <button className="px-24 py-12 bg-blue-600 border-r-3xl">
+            Play now h
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
