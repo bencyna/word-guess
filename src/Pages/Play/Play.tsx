b6a1066fda9f15bfd6335f27d7f2d30524308095
Ben Cyna <benjamincyna@gmail.com>
2022-06-18 07:06:27 +0930

    add generic button class

--- src/Pages/Play/Play.tsx
+++ src/Pages/Play/Play.tsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react'
 import Navbar from "../../Components/Navbar/Navbar.tsx";
-import Input from "../../Components/Input/Input.tsx"
+import Input from "../../Components/HTML/Input.tsx"
+import Button from "../../Components/HTML/Button.tsx"
 
 export default function Play() {
   const [playersNumber, setPlayerNumbers] = useState(0);
   const [play, setPlay] = useState(false);
 
-  const onChange = (e) => {
+  const playersOnChange = (e) => {
     if (e.target.value > 6) {
       e.target.value = 6
     }
@@ -19,9 +20,8 @@ export default function Play() {
       <div className="playersChice">
         {!play ? (
         <div className="md:w-1/2 m-auto md:mt-24 border p-24">
-          <Input type="number" class = {{}} onChange={onChange} value={playersNumber} text="Number of players" name="playersNumber" id="playersNumber" placeholder="0"/>
-        {playersNumber}
-
+          <Input type="number" class = {{}} onChange={playersOnChange} value={playersNumber} text="Number of players" name="playersNumber" id="playersNumber" placeholder="0"/>
+          <Button text="Play"/>
         </div>
         ) : (
         <div>
