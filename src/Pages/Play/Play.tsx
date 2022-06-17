26958a569ede402691fa7cab562aae15f474cb7d
Ben Cyna <benjamincyna@gmail.com>
2022-06-18 06:54:25 +0930

    style for mobilr improved

--- src/Pages/Play/Play.tsx
+++ src/Pages/Play/Play.tsx
@@ -10,10 +10,7 @@ export default function Play() {
       <Navbar />
       <div className="playersChice">
         {playersNumber <= 1 ? (
-        <div className="w-1/2 m-auto mt-24 w-1/2 m-auto border p-24">
-          <h3 className="text-2xl">
-          choose how many players
-          </h3>
+        <div className="md:w-1/2 m-auto md:mt-24 border p-24">
           <Input type="number" class = {{}} value={playersNumber} text="Number of players" name="playersNumber" id="playersNumber" placeholder="0"/>
         </div>
         ) : (
