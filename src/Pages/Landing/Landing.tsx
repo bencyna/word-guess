ab915a2e46b070cdff286a410ff41a9038488c64
Ben Cyna <benjamincyna@gmail.com>
2022-06-17 18:45:05 +0930

    added link to play game from landing page

--- src/Pages/Landing/Landing.tsx
+++ src/Pages/Landing/Landing.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../Components/Navbar/Navbar.tsx";
+import {Link} from "react-router-dom"
 
 export default function Landing() {
   return (
@@ -7,9 +8,9 @@ export default function Landing() {
       <Navbar />
       <div className="w-1/2 m-auto">
         <div id="gameOptions" className="mt-44 w-1/2 m-auto border p-44">
-          <button className="text-white px-24 py-6 bg-blue-600 rounded-3xl relative right-24 hover:text-gray-200">
+          <Link to="/play" className="text-white px-24 py-6 bg-blue-600 rounded-3xl relative right-24 hover:text-gray-200">
             Play Locally
-          </button>
+          </Link>
         </div>
       </div>
     </div>
