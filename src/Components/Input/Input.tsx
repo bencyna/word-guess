5f6b57450d260968c7dd39a3dbde64ad53841abc
Ben Cyna <benjamincyna@gmail.com>
2022-06-18 07:00:38 +0930

    add constraints to input

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -12,6 +12,7 @@ export default function Input(props) {
           id={props.id}
           className={`block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm ${props.class.input}`}
           placeholder={props.placeholder}
+          onChange={props.onChange}
         />
       </div>
     )
--- src/Pages/Play/Play.tsx
+++ src/Pages/Play/Play.tsx
@@ -4,14 +4,24 @@ import Input from "../../Components/Input/Input.tsx"
 
 export default function Play() {
   const [playersNumber, setPlayerNumbers] = useState(0);
+  const [play, setPlay] = useState(false);
+
+  const onChange = (e) => {
+    if (e.target.value > 6) {
+      e.target.value = 6
+    }
+    setPlayerNumbers(e.target.value);
+  }
 
   return (
     <div>
       <Navbar />
       <div className="playersChice">
-        {playersNumber <= 1 ? (
+        {!play ? (
         <div className="md:w-1/2 m-auto md:mt-24 border p-24">
-          <Input type="number" class = {{}} value={playersNumber} text="Number of players" name="playersNumber" id="playersNumber" placeholder="0"/>
+          <Input type="number" class = {{}} onChange={onChange} value={playersNumber} text="Number of players" name="playersNumber" id="playersNumber" placeholder="0"/>
+        {playersNumber}
+
         </div>
         ) : (
         <div>
