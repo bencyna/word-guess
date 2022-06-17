b5c0d5e526af383255229da4ffe4c4aae2164949
Ben Cyna <benjamincyna@gmail.com>
2022-06-18 06:32:46 +0930

    add state to play locally

--- src/Pages/Landing/Landing.tsx
+++ src/Pages/Landing/Landing.tsx
@@ -8,7 +8,7 @@ export default function Landing() {
       <Navbar />
       <div className="w-1/2 m-auto">
         <div id="gameOptions" className="mt-44 w-1/2 m-auto border p-44">
-          <Link to="/play" className="text-white px-24 py-6 bg-blue-600 rounded-3xl relative right-24 hover:text-gray-200">
+          <Link to="/play" className="text-white w-full px-24 py-6 bg-blue-600 rounded-3xl relative hover:text-gray-200">
             Play Locally
           </Link>
         </div>
--- src/Pages/Play/Play.tsx
+++ src/Pages/Play/Play.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Play() {
+  const [playersNumber, setPlayerNumbers] = useState(0);
+
   return (
-    <div>Play</div>
+    <div>
+      {/* How many players? */}
+      <div className="playersChice">
+        {playersNumber < 0 ? (<div>
+          choose how many players
+        </div>) : (<div>
+          player
+        </div>)}
+      </div>
+    </div>
   )
 }
