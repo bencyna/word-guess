4b8ab24a0f10508008a04b7a73ec345508842f75
Ben Cyna <benjamincyna@gmail.com>
2022-06-18 22:04:44 +0930

    comment on how to connect live game

--- src/Pages/GameEngaged/GameEngaged.tsx
+++ src/Pages/GameEngaged/GameEngaged.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function GameEngaged() {
+  // if game is local, connect to created player objects, no need to save data
+  // if game is online. Need to have game key - in database. Store game key, and users figure out how to connect live- should be able to allow access to page with database and path/:id
+
   return (
     <div>GameEngaged</div>
   )
